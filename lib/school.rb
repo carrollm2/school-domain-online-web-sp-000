cde85b93ad087228c70a936bc8c8e3eddd3f37a9
Michael Carroll <carrollm2@gmail.com>
2019-10-27 23:08:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
-# code here!
+require 'pry'
+
+class School
+
+  #GRADES = []
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  def roster
+    @roster
+  end
+
+
+  def add_student(student_name, grade_number)
+    #binding.pry
+    if @roster[grade_number]
+      @roster[grade_number] << student_name
+    else
+      @roster[grade_number] = []
+      @roster[grade_number] << student_name
+    end
+  end
+
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+
+  def sort
+    #sorted_names = {}
+    #@roster.each do |grade, students|
+    #  sorted_names[grade] = []
+    #  sorted_names[grade] << students.sort
+    #end
+    #sorted_names
+    @roster.each do |grade|
+      @roster[grade].sort
+    end
+    @roster
+
+  end
+end
